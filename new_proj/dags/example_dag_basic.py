1bb99357eb93644d8035afe30f6679bcf3db38b6
Christine Shen <christine.shen@astronomer.io>
2024-12-19 12:53:22 -0500

    edit untracked project

--- new_proj/dags/example_dag_basic.py
+++ new_proj/dags/example_dag_basic.py
@@ -21,7 +21,7 @@ from pendulum import datetime
     # run will be for the next 30 mins, per the its schedule
     catchup=False,
     default_args={
-        "retries": 2,  # If a task fails, it will retry 2 times.
+        "retries": 3,  # If a task fails, it will retry 3 times.
     },
 )  # If set, this tag is shown in the DAG view of the Airflow UI
 def example_dag_basic():
